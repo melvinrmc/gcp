044328dc7eb94590ae5f9f6e1cdf0fa18d56c386
melvinrmc <melvinrmc@hotmail.com>
2020-02-04 14:21:52 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -42,7 +42,7 @@ resource "google_container_node_pool" "primary_preemptible_nodes" {
   name       = "my-node-pool"
   location   = var.MY_REGION
   cluster    = google_container_cluster.primary.name
-  node_count = 1
+  node_count = 2
 
   node_config {
     preemptible  = true
